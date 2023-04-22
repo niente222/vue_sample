7ef69e141d61c31a10009d84d73401646086b580
niente <komanagi12345@gmail.com>
2023-04-22 16:53:58 +0900

    tooltip実装

--- src/components/pages/page2.vue
+++ src/components/pages/page2.vue
@@ -1,5 +1,7 @@
 <template>
     <h1>ラストワンゲーム</h1>
+    <input type="text" v-model="playerName" />
+    <br>
     <button id="unlockButton" @click="handleClick">数字を解除する</button>
     <div id="result"></div>
 
@@ -7,7 +9,7 @@
     <table class="bingo-board">
       <tbody>
         <tr v-for="(row, rowIndex) in bingoNumbers" :key="rowIndex">
-          <td class="bingo-cell" v-for="(number, colIndex) in row" :key="colIndex" :id="'bingo-cell-' + number">{{ number }}</td>
+          <td class="bingo-cell" v-for="(number, colIndex) in row" :key="colIndex" :id="'bingo-cell-' + number">{{ number }}<div class='tooltip-text' :id="'tooltip-' + number"></div></td>
         </tr>
       </tbody>
     </table>
@@ -24,7 +26,8 @@
     name: 'page2',
     data(){
       return{
-        bingoNumbers: []
+        bingoNumbers: [],
+        playerName : ''
       }
     },
 
@@ -33,24 +36,32 @@
       lockStatus = Array(10).fill(true);
       unlockButton = document.getElementById('unlockButton');
       resultDiv = document.getElementById('result');
-      
-      socket.addEventListener('open', () => {
+
+      if(socket.readyState === WebSocket.OPEN){
         socket.send("");
-      });
-    
-      socket.addEventListener('message', (event) => {
+      }else{
+        socket.addEventListener('open', () => {
+          socket.send("");
+        });
+      
+        socket.addEventListener('message', (event) => {
           const receivedArray = JSON.parse(event.data);
           console.log('Received array:', receivedArray);
-          receivedArray.forEach(num => {
-            this.markNumber(num);
-          })
-      });
-  
-      socket.addEventListener('close', (event) => {
-          console.log('WebSocket connection closed:', event);
-      });
+          receivedArray.forEach(str => {
+            const firstDollarSignIndex = str.indexOf('$');
+            const secondDollarSignIndex= str.indexOf('$', firstDollarSignIndex + 1);
+            const num = str.substring(0, firstDollarSignIndex);
+            const playerName = secondDollarSignIndex !== -1 ? str.substring(firstDollarSignIndex, secondDollarSignIndex).replace(/\$/g, '') : str;
+            const date = secondDollarSignIndex !== -1 ? str.substring(secondDollarSignIndex).replace(/\$/g, '') : '';
 
-  
+            this.markNumber(num,playerName,date);
+          })
+        });
+    
+        socket.addEventListener('close', (event) => {
+          console.log('WebSocket connection closed:', event);
+        });
+      }
     },
 
     created() {
@@ -64,7 +75,12 @@
         const randomIndex = Math.floor(Math.random() * lockedNumbers.length);
         const selectedNumber = lockedNumbers[randomIndex];
 
-        socket.send(selectedNumber);
+        if(this.playerName.length === 0){
+          this.playerName = 'No Name';
+        }
+
+        this.playerName = this.playerName.replace(/\$/g, '');
+        socket.send(selectedNumber + '$' + this.playerName + '$' + this.getDateFormat(new Date()));
 
         if (lockedNumbers.length === 1) {
             resultDiv.textContent = 'おめでとうございます！あなたが最後の数字 ' + selectedNumber + ' を解除しました！';
@@ -84,15 +100,37 @@
           }
         }
       },
-      markNumber(number) {
+      markNumber(number,playerName,date) {
         const cellId = `bingo-cell-${number}`;
         const cell = document.getElementById(cellId);
+        const tooltipId = `tooltip-${number}`;
+        const tooltip = document.getElementById(tooltipId);
 
         if (cell) {
           cell.style.backgroundColor = 'yellow';
+          tooltip.innerHTML = `${number}<br>${playerName}<br>${date}`;
+          console.log(playerName + date);
         } else {
           //予期せぬ値
         }
+      },
+      getDateFormat(date){
+        const y = date.getFullYear();
+        const M = date.getMonth() + 1;
+        const d = date.getDate();
+        const h = date.getHours();
+        const m = date.getMinutes();
+        const s = date.getSeconds();
+    
+    
+        const yyyy = y.toString();
+        const MM = ("00" + M).slice(-2);
+        const dd = ("00" + d).slice(-2);
+        const hh = ("00" + h).slice(-2);
+        const mm = ("00" + m).slice(-2);
+        const ss = ("00" + s).slice(-2);
+
+        return yyyy + '/' + MM + '/' + dd + ' ' + hh + ':'  + mm + ':'  + ss;
       }
     },
   }
